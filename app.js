56bb985989a135dec26e5802e4368636f4437257
Joe Naiburg <jnaiburg@stevens.edu>
2020-04-29 15:19:28 -0400

    edited app.js

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ const static = express.static(__dirname + '/public')
 
 const configRoutes = require('./routes')
 const exphbs = require('express-handlebars')
+
+app.use('/public', static)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+configRoutes(app)
+
+app.listen(3000, () => {
+    console.log("We have a server")
+    console.log('Routes will be running on http://localhost:3000')
+})
