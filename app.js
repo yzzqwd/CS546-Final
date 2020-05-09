e4fd4397c5c147acd40abeaf12eb7a436b6f0455
atags314 <atags314@gmail.com>
2020-05-09 15:47:21 -0400

    Bootstrap

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const app = express()
-const static = express.static(__dirname + '/public')
+const express = require('express');
+const app = express();
 
-const configRoutes = require('./routes')
-const exphbs = require('express-handlebars')
+const configRoutes = require('./routes');
+const exphbs = require('express-handlebars');
 
-app.use('/public', static)
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+ 
+const static = express.static(__dirname + '/public');
+app.use('/public', static);
 
-configRoutes(app)
-
+configRoutes(app);
 app.listen(3000, () => {
-    console.log("We have a server")
-    console.log('Routes will be running on http://localhost:3000')
-})
+	console.log("We've now got a server!");
+	console.log('Your routes will be running on http://localhost:3000');
+});
