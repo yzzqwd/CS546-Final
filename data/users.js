e89012a8f40351727f8797d4af53a548feaba073
yzzqwd <771242023@qq.com>
2020-05-09 21:35:27 -0400

    Add files via upload

--- data/users.js
+++ data/users.js
@@ -51,5 +51,30 @@ module.exports = {
 			throw `Could not delete user with id of ${id}`;
 		}
 		return user;
-    }
+	},
+	async updateUser(userId,firstName, lastName, username,gender,email,city,state,age) {
+		if (!userId) throw 'You must provide userId';
+		if (!firstName) throw 'You must provide a firstname';
+        if (!lastName) throw 'You must provide a lastname';
+        if (!username) throw 'You must provide a username';
+        if (!gender) throw 'You must provide gender';
+        if (!email) throw 'You must provide email';
+        if (!city) throw 'You must provide city';
+        if (!state) throw 'You must provide state';
+        if (!age || typeof(age) !== 'number') throw 'You must provide a vaild age';
+		const usersCollection = await users();
+		let newUser = {
+			firstName: fristName,
+            lastName: lastName,
+            username:username,
+            gender:gender,
+            email:email,
+            city:city,
+            state:state,
+            age:age
+		};
+		const updatedInfo = await usersCollection.updateOne({_id:userId},{$set:newUser});
+		if (updatedInfo.modifiedCount === 0) throw 'Could not update user';
+		return await this.get(userId);
+	}
 }
