63019a186b497796e2a769110fd3e8c2d18fa7b0
yzzqwd <771242023@qq.com>
2020-05-11 20:44:31 -0400

    Add files via upload

--- tasks/seed.js
+++ tasks/seed.js
@@ -6,10 +6,7 @@ const admin = data.admin
 async function main() {
 	const db = await dbConnection();
 	await db.dropDatabase();
-
-    const patrick = await admin.create('Patrick', 'Hill', 'phill546', 'Male', 'phill@stevens.edu', 'Hoboken', 'New Jersey', '30', '$2y$15$uSYzHNt2iabWJPlxO57J2.2knV.43SxxuZQPxmX3krPMnKNfG7tgy');
-    
-    const john = await users.create('John', 'Smith','johnsmith1234', 'Male', 'jsmith@gmail.com', 'Albany', 'New York', '20', '$2y$15$DtoFWeXCmMYpS4khcj2d3uzbHn6r45NDoq9HU8MgiC5UMYGpK/ngy');
+    const john = await admin.create('johnsmith1234',hashedPassword);
 	const id = john._id;
     
     //await posts.create(id, img, caption, time);
