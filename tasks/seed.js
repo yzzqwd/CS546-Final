83c299304ac37e0cdd606311f6fa81d950d1fa45
Chaois <chuqiushi@outlook.com>
2020-05-11 22:45:26 -0400

    modify seed

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,19 +1,20 @@
 const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
 const users = data.users;
-const admin = data.admin
+const admin = data.admin;
+const bcrypt = require('bcryptjs');
 
-async function main() {
+const main = async () => {
 	const db = await dbConnection();
 	await db.dropDatabase();
-    const john = await admin.create('johnsmith1234',hashedPassword);
-	const id = john._id;
-    
-    //await posts.create(id, img, caption, time);
 
-	await db.serverConfig.close();
-	
+	const adName = 'admin';
+	const adPassword = 'PatrickHill';
+	const adHash = await bcrypt.hash(adPassword, 15);
+    await admin.create(adName, adHash);
+    
 	console.log('Done!');
+	await db.serverConfig.close();
 }
 
-main();
+main().catch(console.log);
